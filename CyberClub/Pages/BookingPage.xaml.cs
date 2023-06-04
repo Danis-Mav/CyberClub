6139fc76c37a2c02a89160d316d3b36a9020ef5b
Danis <Danis@DMEOWTWO>
2023-06-04 20:34:10 +0300

    Добавьте файлы проекта.

---
+++ CyberClub/Pages/BookingPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CyberClub.DB;
+using Table = CyberClub.DB.Table;
+
+namespace CyberClub.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для BookingPage.xaml
+    /// </summary>
+    public partial class BookingPage : Page
+    {
+        private bool isMenuOpen;
+        public static ObservableCollection<Table> table { get; set; }
+        public bool IsMenuOpen
+        {
+            get { return isMenuOpen; }
+            set
+            {
+                isMenuOpen = value;
+                OnPropertyChanged(nameof(IsMenuOpen));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public static User User { get; set; }
+        public static Table Table { get; set; }
+        public BookingPage(User user)
+        {
+            InitializeComponent();
+            User = user;
+            table = new ObservableCollection<Table>(DBConnection.connection.Table.Where(x => (x.IsDeleted == false) && (x.IsDeleted == false)).ToList());
+            DataContext = user;
+            this.DataContext = this;
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ToggleMenu(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpen = !IsMenuOpen;
+            menuPanel.Visibility = IsMenuOpen ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void NavigateToNewsPage(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpen = false;
+            NavigationService.Navigate(new NewsPage(User));
+        }
+
+        private void NavigateToEventsPage(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpen = false;
+            NavigationService.Navigate(new EventsPage(User));
+        }
+
+        private void NavigateToBookingPage(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpen = false;
+            NavigationService.Navigate(new BookingPage(User));
+        }
+
+        private void NavigateToTariffPage(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpen = false;
+            NavigationService.Navigate(new TariffPage(User));
+        }
+
+        private void NavigateToChatPage(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpen = false;
+            NavigationService.Navigate(new ChatPage(User));
+        }
+        private void NavigateToAboutPage(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpen = false;
+            NavigationService.Navigate(new MainPage(User));
+        }
+        private void NavigateToProfilePage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ProfilePage(User));
+        }
+    }
+}
